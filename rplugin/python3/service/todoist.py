d6f7d0cfa13dfc8384d2500338643447fe079658
sakochik <sakochi.kodai1209@gmail.com>
2018-11-10 11:57:57 +0900

    :alien:add: service layer

---
+++ rplugin/python3/service/todoist.py
@@ -0,0 +1,17 @@
+import sys
+sys.path.append('/Users/sakochikodai/Desktop/todoist_nvim')
+
+from rplugin.python3.model.user import UserDomainService
+
+class TodoistService:
+    def __init__(self, email: str, password: str) -> None:
+        self.email = email
+        self.password = password
+
+    def get_project(self, project_name: str):
+        user = UserDomainService.get_original_user(self.email, self.password)
+        return user.get_project(project_name)
+
+    def get_all_projects(self):
+        user = UserDomainService.get_original_user(self.email, self.password)
+        return user.get_projects()
