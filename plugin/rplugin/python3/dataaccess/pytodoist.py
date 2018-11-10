84f75a0ab33cdd647cc9298f9256af1f79e44cd9
sakochi_koudai <sakochi_koudai@aratana.jp>
2018-11-10 17:40:03 +0900

    add: dataaccess layer and move function from model

---
+++ plugin/rplugin/python3/dataaccess/pytodoist.py
@@ -0,0 +1,21 @@
+from pytodoist import todoist
+
+from rplugin.python3.anticorruption.todoist import TodoistUserTransfer
+
+class PytodoistAPIDataSource:
+    def __init__(self, email: str, password: str) -> None:
+        self.email = email
+        self.password = password
+
+    def get_user(self) -> todoist.User:
+        response = todoist.login(self.email, self.password)
+        transfer = TodoistUserTransfer(response)
+        return transfer.to_my_user()
+
+    def get_project(self, project_name: str) -> todoist.Project:
+        user = self.get_user()
+        return user.get_project(project_name)
+
+    def get_projects(self) -> list(todoist.Project):
+        user = self.get_user()
+        return user.get_projects()
