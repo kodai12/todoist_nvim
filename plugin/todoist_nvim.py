5b7539db1ba05d49f31e2c426c5e46cf8762edb5
sakochi_koudai <sakochi_koudai@aratana.jp>
2018-11-10 15:17:41 +0900

    modify: import neccessary plugin & apply formatter

--- plugin/todoist_nvim.py
+++ plugin/todoist_nvim.py
@@ -1,9 +1,11 @@
 import vim
-from rplugin.python3.service.todoist import TodoistService
+from ..rplugin.python3.service.todoist import TodoistService
+
 
 def get_project(project_name: str):
     service = TodoistService(_get_email(), _get_password())
     return service.get_project(project_name)
+
 
 def get_all_projects():
     service = TodoistService(_get_email(), _get_password())
@@ -11,20 +13,24 @@ def get_all_projects():
     for project in projects:
         print(project.name)
 
+
 def _get_api_token():
     token_exists = int(vim.eval('exists("g:todoist_api_token")'))
 
     if token_exists:
         return vim.eval('g:todoist_api_token')
     else:
-        print("API Token is not set. Please type: let g:todoist_api_token = 'YOUR_TOKEN'")
+        print("API Token is not set. Please type: \
+              let g:todoist_api_token = 'YOUR_TOKEN'")
         return 0
+
 
 def _get_email():
     email_exists = vim.eval('exists("g:todoist_email")')
 
     if email_exists:
         return vim.eval('g:todoist_email')
+
 
 def _get_password():
     password_exists = vim.eval('exists("g:todoist_password")')
--- plugin/todoist_nvim.vim
+++ plugin/todoist_nvim.vim
@@ -2,8 +2,8 @@
 " Add our plugin to the path
 " --------------------------------
 python import sys
+python import vim
 python sys.path.append(vim.eval('expand("<sfile>:p:h")'))
-python import todoist_nvim
 
 " --------------------------------
 "  Function(s)
