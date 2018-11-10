9b861e852de4760327047ff1e769a6038b6f66b1
sakochi_koudai <sakochi_koudai@aratana.jp>
2018-11-10 17:09:59 +0900

    modify: create base User class(Entity)

--- plugin/rplugin/python3/model/user.py
+++ plugin/rplugin/python3/model/user.py
@@ -1,32 +1,66 @@
+from datetime import datetime
 
-from rplugin.python3.model.project import Project
-from pytodoist import todoist
-import sys
-sys.path.append('/Users/sakochikodai/Desktop/todoist_nvim')
-
-
-class UserDomainService:
-    @staticmethod
-    def get_user(email: str, password: str) -> object:
-        original_user = todoist.login(email, password)
-        return User(original_user)
-
-    @staticmethod
-    def get_original_user(email: str, password: str) -> object:
-        return todoist.login(email, password)
+from rplugin.python3.model.entity import Entity
+from rplugin.python3.model.value import Value
 
 
-class User(todoist.User):
-    def __init__(self) -> None:
-        pass
+class UserId(Value):
+    def __init__(self, value: int) -> None:
+        self.user_id = value
+
+
+class Email(Value):
+    def __init__(self, value: str) -> None:
+        self.email = value
+
+
+class Password(Value):
+    def __init__(self, value: str) -> None:
+        self.password = value
+
+
+class FullName(Value):
+    def __init__(self, value: str) -> None:
+        self.full_name = value
+
+
+class PremiumStatus(Value):
+    def __init__(self, is_premium:bool, premium_until: datetime) -> None:
+        self.is_premium = is_premium
+        self.premium_until = premium_until
+
+
+class StartDay(Value):
+    def __init__(self, value: datetime) -> None:
+        self.start_day = value
+
+
+class ApiToken(Value):
+    def __init__(self, value: str):
+        self.api_token = value
+
+
+class User(Entity):
+    def __init__(self,
+                 user_id: UserId,
+                 email: Email,
+                 password: Password,
+                 full_name: FullName,
+                 premium_status: PremiumStatus,
+                 start_day: StartDay,
+                 api_token: ApiToken) -> None:
+        self.user_id = user_id,
+        self.email = email,
+        self.password = password,
+        self.full_name = full_name,
+        self.premium_status = premium_status,
+        self.start_day = start_day,
+        self.api_token = api_token,
 
     def get_project(self, project_name: str):
-        project_orig = super().get_project(project_name)
-        return Project(project_orig)
+        pass
 
     def get_all_project(self):
-        projects_orig = super().get_projects()
-        projects: list(Project) = []
-        for project_orig in projects_orig:
-            projects.append(Project(project_orig))
-        return projects
+        pass
+
+
