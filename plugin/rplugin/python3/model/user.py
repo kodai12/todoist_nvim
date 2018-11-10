f50a491cf4ece9771ad9d224189638a363496a7f
sakochi_koudai <sakochi_koudai@aratana.jp>
2018-11-10 17:10:21 +0900

    add: factory => User class

--- plugin/rplugin/python3/model/user.py
+++ plugin/rplugin/python3/model/user.py
@@ -64,3 +64,21 @@ class User(Entity):
         pass
 
 
+def create_user(user_id: int,
+                email: str,
+                password: str,
+                full_name: str,
+                is_premium: bool,
+                premium_until: datetime,
+                start_day: datetime,
+                api_token: str):
+    return User(
+        user_id=UserId(user_id),
+        email=Email(email),
+        password=Password(password),
+        full_name=FullName(full_name),
+        premium_status=PremiumStatus(is_premium, premium_until),
+        start_day=StartDay(start_day),
+        api_token=ApiToken(api_token),
+    )
+
