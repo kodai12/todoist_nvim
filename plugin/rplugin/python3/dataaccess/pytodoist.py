cddec18dd840fe7459f5180c674cfed895fd877a
kodai12 <sakochi.kodai1209@gmail.com>
2018-11-11 18:01:38 +0900

    modify: move transfer process from datasource layer to application layer

--- plugin/rplugin/python3/dataaccess/pytodoist.py
+++ plugin/rplugin/python3/dataaccess/pytodoist.py
@@ -1,39 +1,23 @@
 from pytodoist import todoist
 
-from rplugin.python3.anticorruption.user import TodoistUserTransfer
-from rplugin.python3.anticorruption.project import TodoistProjectTransfer
-from rplugin.python3.anticorruption.project import TodoistProjectListTransfer
-from rplugin.python3.anticorruption.task import TodoistTaskListTransfer
-
-from rplugin.python3.model.user import User
-from rplugin.python3.model.project import Project
-
 
 class PytodoistAPIDataSource:
     def __init__(self, email: str, password: str) -> None:
         self.email = email
         self.password = password
 
-    def get_user(self) -> User:
-        orig_user = todoist.login(self.email, self.password)
-        transfer = TodoistUserTransfer(orig_user)
-        return transfer.to_my_user()
+    def get_user(self) -> todoist.User:
+        return todoist.login(self.email, self.password)
 
-    def get_project(self, project_name: str) -> Project:
+    def get_project(self, project_name: str) -> todoist.Project:
         orig_user = todoist.login(self.email, self.password)
-        orig_project = orig_user.get_project(project_name)
-        transfer = TodoistProjectTransfer(orig_project)
-        return transfer.to_my_project()
+        return orig_user.get_project(project_name)
 
     def get_all_projects(self) -> list:
         orig_user = todoist.login(self.email, self.password)
-        orig_projects = orig_user.get_projects()
-        transfer = TodoistProjectListTransfer(orig_projects)
-        return transfer.to_my_projects()
+        return orig_user.get_projects()
 
     def get_all_tasks(self, project_name: str) -> list:
         orig_user = todoist.login(self.email, self.password)
         orig_project = orig_user.get_project(project_name)
-        orig_tasks = orig_project.get_tasks()
-        transfer = TodoistTaskListTransfer(orig_tasks)
-        return transfer.to_my_tasks()
+        return orig_project.get_tasks()
--- plugin/rplugin/python3/service/todoist.py
+++ plugin/rplugin/python3/service/todoist.py
@@ -2,19 +2,32 @@ from rplugin.python3.dataaccess.pytodoist import PytodoistAPIDataSource
 from rplugin.python3.model.user import User
 from rplugin.python3.model.project import Project
 
+from rplugin.python3.anticorruption.user import TodoistUserTransfer
+from rplugin.python3.anticorruption.project import TodoistProjectTransfer
+from rplugin.python3.anticorruption.project import TodoistProjectListTransfer
+from rplugin.python3.anticorruption.task import TodoistTaskListTransfer
+
 
 class TodoistService:
     def __init__(self, datasouce: PytodoistAPIDataSource):
         self.datasouce = datasouce
 
     def get_user(self) -> User:
-        return self.datasouce.get_user()
+        orig_user = self.datasouce.get_user()
+        transfer = TodoistUserTransfer(orig_user)
+        return transfer.to_my_user()
 
     def get_project(self, project_name: str) -> Project:
-        return self.datasouce.get_project(project_name)
+        orig_project = self.datasouce.get_project(project_name)
+        transfer = TodoistProjectTransfer(orig_project)
+        return transfer.to_my_project()
 
     def get_all_projects(self) -> list:
-        return self.datasouce.get_all_projects()
+        orig_projects = self.datasouce.get_all_projects()
+        transfer = TodoistProjectListTransfer(orig_projects)
+        return transfer.to_my_projects()
 
     def get_all_tasks(self, project_name: str) -> list:
-        return self.datasouce.get_all_tasks(project_name)
+        orig_tasks = self.datasouce.get_all_tasks(project_name)
+        transfer = TodoistTaskListTransfer(orig_tasks)
+        return transfer.to_my_tasks()
