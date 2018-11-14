9c9f8a8fd8cfeb983b3cc9cef39e7160a848015f
kodai12 <sakochi.kodai1209@gmail.com>
2018-11-14 22:06:17 +0900

    modify: リマインダー取得件数0件の時の処理

--- plugin/rplugin/python3/interface/todoist_nvim.py
+++ plugin/rplugin/python3/interface/todoist_nvim.py
@@ -121,8 +121,9 @@ def get_all_reminders(args: str):
         return
     datasource = PytodoistAPIDataSource(_get_email(), _get_password())
     service = TodoistQueryService(datasource)
-    print('args[0] is {}, args[1] is {}'.format(args['project'], args['args']))
     reminders = service.get_all_reminders(args['project'], args['args'])
+    if len(remindes) == 0:
+        print('リマインダーは設定されていません')
     for reminder in reminders:
         print('Id: {}\n関連タスクId: {}\nリマインド方法: {}\nDueDate:{}\n============================='
               .format(reminder.reminder_id.value,
